ac50194e59442d59cb491c7875a61434cc0c90c2
Remi Dupin <remi.dupin@etud.univ-pau.fr>
2022-05-11 11:57:47 +0200

    Debut V2

--- chifoumivue.cpp
+++ chifoumivue.cpp
@@ -32,111 +32,198 @@ void chifoumiVue::demarerPartie()
     //Demarage d'une nouvelle partie
     ui ->ImageChoixUtili->setPixmap(QPixmap(QString::fromUtf8(":/images/images/rien_115.png")));    //Remise à rien de l'affichage pour le joueur
     ui ->ImageChoixMachi->setPixmap(QPixmap(QString::fromUtf8(":/images/images/rien_115.png")));    //Remise à rien de l'affichage pour le joueur
-    ui ->ChoixFigure->setEnabled(true);                                                             //Activation des boutons pour le choix de la figure
-    ui ->ScoreVous->setText("0");                                                                   //Remise à 0 du score du joueur
-    ui ->ScoreMachine->setText("0");                                                                //Remise à 0 du score de la machine
+    ui ->ChoixFigure->setEnabled(true);
+    initCoups();
+    initCoups();
+    valJoueurScore = QString::number(scoreJoueur);
+    ui ->ScoreVous->setText(valJoueurScore);
+    valMachineScore = QString::number(scoreMachine);
+    ui ->ScoreMachine->setText(valMachineScore);
 }
 
-//Quand le joueur joue pierre
 void chifoumiVue::coupPierre()
 {
-    ui ->ImageChoixUtili->setPixmap(QPixmap(QString::fromUtf8(":/images/images/pierre_115.png")));  //Affichage de l'image pierre dans la zone du coup du joueur
-    int coupJoueur = 1;                                                                             //nombre qui permet d'indiquer quel coup a fait le joueur [1 = pierre]
-    coupMachine(coupJoueur);                                                                        //Appel du programme permettant de faire le coup de la machine
+    ui ->ImageChoixUtili->setPixmap(QPixmap(QString::fromUtf8(":/images/images/pierre_115.png")));
+    setCoupJoueur(2);
+    deroulementPartie();
 }
 
-//Quand le joueur joue ciseau
 void chifoumiVue::coupCiseau()
 {
-    ui ->ImageChoixUtili->setPixmap(QPixmap(QString::fromUtf8(":/images/images/ciseau_115.png")));  //Affichage de l'image ciseau dans la zone du coup du joueur
-    int coupJoueur = 2;                                                                             //nombre qui permet d'indiquer quel coup a fait le joueur [2 = ciseau]
-    coupMachine(coupJoueur);                                                                        //Appel du programme permettant de faire le coup de la machine
+    ui ->ImageChoixUtili->setPixmap(QPixmap(QString::fromUtf8(":/images/images/ciseau_115.png")));
+    setCoupJoueur(3);
+    deroulementPartie();
 }
 
-//Quand le joueur joue papier
 void chifoumiVue::coupPapier()
 {
-    ui ->ImageChoixUtili->setPixmap(QPixmap(QString::fromUtf8(":/images/images/papier_115.png")));  //Affichage de l'image papier dans la zone du coup du joueur
-    int coupJoueur = 3;                                                                             //nombre qui permet d'indiquer quel coup a fait le joueur [3 = papier]
-    coupMachine(coupJoueur);                                                                        //Appel du programme permettant de faire le coup de la machine
+    ui ->ImageChoixUtili->setPixmap(QPixmap(QString::fromUtf8(":/images/images/papier_115.png")));
+    setCoupJoueur(1);
+    qDebug () << "testT " << coupJoueur;
+    deroulementPartie();
 }
 
-//fonction permettant d'indiquer le coup de la machine et de determiner le gagnant
-void chifoumiVue::coupMachine(int numJoueur)
+
+
+chifoumiVue::UnCoup chifoumiVue::getCoupJoueur()
 {
-    char resu = 'E';                //Variables contenant une lettre pour determiner qui gagne
-    QString valMachineScore;        //Decalarartion d'un variable QString pour contenir la valeur du score de la machine
-    QString valJoueurScore;         //Decalarartion d'un variable QString pour contenir la valeur du score du joueur
-
-    int numMachine = randMinMax(1,4);               //fonction permettant de determiner le coup de la machine de maniere aléatoire
-    resu = determineGagnat(numJoueur,numMachine);   //fonction permettant de determiner qui a gagner entre le joueur et la machine
-
-    if (resu == 'M')    //Le cas ou la machine gagne
-    {
-        valMachineScore = ui ->ScoreMachine->text();    //Récuperation du score de la machine se trouvant dans le Label ScoreMachine pour le mettre dans valMachineScore
-        int num = valMachineScore.toInt();              //On le transforme le score en INT et on le stock dans num pour pouvoir le manipuler
-        num ++;                                         //On incremente num de 1 le Score
-        valMachineScore = QString::number(num);         //On transforme num en STRING et on stocke le score dans valMachineScore pour pouvoir l'afficher
-        ui->ScoreMachine->setText(valMachineScore);     //On Met dans le Label ScoreMachine la valeur contenue dans valMachineScore
-    }
-    if(resu =='J')      //Le cas ou le joueur gagne
-    {
-        valJoueurScore = ui->ScoreVous->text();         //Récuperation du score du joueur se trouvant dans le Label ScoreVous pour le mettre dans valJoueurScore
-        int num = valJoueurScore.toInt();               //On le transforme le score en INT et on le stock dans num pour pouvoir le manipuler
-        num ++;                                         //On incremente num de 1 le Score
-        valJoueurScore = QString::number(num);          //On transforme num en STRING et on stocke le score dans valJoueurScore pour pouvoir l'affichert
-        ui->ScoreVous->setText(valJoueurScore);         //On Met dans le Label ScoreVous la valeur contenue dans valJoueurScore
-    }
+    return coupJoueur;
 }
 
-int chifoumiVue::randMinMax(int min, int max)       //Fonction permettant de determiner le score de la machine aléatoirement
+chifoumiVue::UnCoup chifoumiVue::getCoupMachine()
+{
+    return coupMachine;
+}
+
+unsigned int chifoumiVue::getScoreJoueur()
+{
+    return scoreJoueur;
+}
+
+unsigned int chifoumiVue::getScoreMachine()
+{
+    return scoreMachine;
+}
+
+char chifoumiVue::determinerGagnant()
+{
+    qDebug() << "Debut determiner gagnant " << Qt::endl;
+    char gagnantARetourner;
+
+    // avant de commencer : match nul
+    gagnantARetourner = 'N';
+    UnCoup joueur = getCoupJoueur();
+    UnCoup machine = getCoupMachine();
+
+    qDebug () << machine << " " << joueur ;
+    if (machine == UnCoup::papier && joueur == UnCoup::ciseau) {
+        gagnantARetourner = 'J';
+        qDebug() << "Cas 1 " << Qt::endl;
+    }
+    if (machine == UnCoup::papier && joueur == UnCoup::pierre) {
+        gagnantARetourner = 'M';
+        qDebug() << "Cas 2 " << Qt::endl;
+    }
+    if (machine == UnCoup::pierre && joueur == UnCoup::ciseau) {
+        gagnantARetourner = 'M';
+        qDebug() << "Cas 3 " << Qt::endl;
+    }
+    if (machine == UnCoup::pierre && joueur == UnCoup::papier) {
+        gagnantARetourner = 'J';
+        qDebug() << "Cas 4 " << Qt::endl;
+    }
+    if (machine == UnCoup::ciseau && joueur == UnCoup::pierre) {
+        gagnantARetourner = 'J';
+        qDebug() << "Cas 5 " << Qt::endl;
+    }
+    if (machine == UnCoup::ciseau && joueur == UnCoup::papier) {
+        gagnantARetourner = 'M';
+        qDebug() << "Cas 6 " << Qt::endl;
+    }
+    qDebug() << "Fin determiner gagnant " << Qt::endl;
+    return gagnantARetourner;
+}
+
+//Fonction permettant de determiner le score de la machine aléatoirement
+int randMinMax(int min, int max)
 {
      /*pré-condition : min<max, Le nbre aléatoire est compris entre [min, max[*/
     return rand()%(max-min) + min;
 }
 
-char chifoumiVue::determineGagnat(int joueur, int machine)      //Fonction qui permet de dertimener qui gagner entre le joueur et la machine
-{
-    char gagnantARetourner = 'E';
 
-    if (machine == 1)       //Affichage de l'image pierre dans la zone du coup de machine
+void chifoumiVue::setCoupJoueur(int num)
+{
+    qDebug () << num << Qt::endl;
+    if (num == 1)
     {
-        ui ->ImageChoixMachi->setPixmap(QPixmap(QString::fromUtf8(":/images/images/pierre_115.png")));
+        qDebug () << "init papier";
+        coupJoueur = UnCoup::papier;
     }
-    if (machine == 2)       //Affichage de l'image ciseau dans la zone du coup de machine
+    if (num == 2)
     {
-        ui ->ImageChoixMachi->setPixmap(QPixmap(QString::fromUtf8(":/images/images/ciseau_115.png")));
+        coupJoueur = UnCoup::pierre;
     }
-    if (machine == 3)       //Affichage de l'image joueur dans la zone du coup de machine
+    if (num == 3)
+    {
+        coupJoueur = UnCoup::ciseau;
+    }
+
+}
+
+//fonction permettant d'indiquer le coup de la machine et de determiner le gagnant
+void chifoumiVue::setCoupMachine()
+{
+    int num;
+    num = randMinMax(1,4);
+
+    if (num == 1)
     {
         ui ->ImageChoixMachi->setPixmap(QPixmap(QString::fromUtf8(":/images/images/papier_115.png")));
+        coupMachine = UnCoup::papier;
     }
+    if (num == 2)
+    {
+        ui ->ImageChoixMachi->setPixmap(QPixmap(QString::fromUtf8(":/images/images/pierre_115.png")));
+        coupMachine = UnCoup::pierre;
+    }
+    if (num == 3)
+    {
+        ui ->ImageChoixMachi->setPixmap(QPixmap(QString::fromUtf8(":/images/images/ciseau_115.png")));
+        coupMachine = UnCoup::ciseau;
+    }
+}
 
+void chifoumiVue::setScoreJoueur(unsigned int p_score)
+{
+    scoreJoueur = scoreJoueur + p_score;
+    valJoueurScore = QString::number(scoreJoueur);
+    ui ->ScoreVous->setText(valJoueurScore);
 
-    if (machine == 3 && joueur == 2)    //cas où si le joueur joue ciseau et la machine papier
-    {
-        gagnantARetourner = 'J';        //Le joueur gagne
-    }
-    if (machine == 3 && joueur == 1)    //cas où si le joueur joue pierre et la machine papier
-    {
-        gagnantARetourner = 'M';        //La machine gagne
-    }
-    if (machine == 1 && joueur == 2)    //cas où si le joueur joue ciseau et la machine pierre
-    {
-        gagnantARetourner = 'M';        //La machine gagne
-    }
-    if (machine == 1 && joueur == 3)    //cas où si le joueur joue papier et la machine pierre
-    {
-        gagnantARetourner = 'J';        //Le joueur gagne
-    }
-    if (machine == 2 && joueur == 1)    //cas où si le joueur joue pierre et la machine ciseau
-    {
-        gagnantARetourner = 'J';        //Le joueur gagne
-    }
-    if (machine == 2 && joueur == 3)    //cas où si le joueur joue papier et la machine ciseau
-    {
-        gagnantARetourner = 'M';        //La machine gagne
-    }
+}
 
-    return gagnantARetourner;   //Renvoie du resultat au programme appelant
+void chifoumiVue::setScoreMachine(unsigned int p_score)
+{
+    scoreMachine = scoreMachine + p_score;
+
+    valMachineScore = QString::number(scoreMachine);
+    ui ->ScoreMachine->setText(valMachineScore);
+}
+
+void chifoumiVue::majScores(char p_gagnant)
+{
+    if (p_gagnant == 'J')
+    {
+        qDebug() << "gagnant joueur " << Qt::endl;
+        setScoreJoueur(1);
+
+    }
+    if (p_gagnant == 'M')
+    {
+        qDebug() << "gagnant machine " << Qt::endl;
+        setScoreMachine(1);
+    }
+}
+
+void chifoumiVue::initScores()
+{
+    scoreJoueur = 0;
+    scoreMachine = 0;
+}
+
+void chifoumiVue::initCoups()
+{
+    coupJoueur = rien;
+    coupMachine = rien;
+}
+
+void chifoumiVue::deroulementPartie()
+{
+    qDebug() << "debut deroulement " << Qt::endl;
+    char p_gagnant;
+    setCoupMachine();
+    p_gagnant = determinerGagnant();
+
+    majScores(p_gagnant);
+    qDebug() << "fin deroulement " << Qt::endl;
 }
--- chifoumivue.h
+++ chifoumivue.h
@@ -15,16 +15,69 @@ public:
     chifoumiVue(QWidget *parent = nullptr);
     ~chifoumiVue();
 
-private slots:
-    void demarerPartie();           //Slot pour commencer la partie
-    void coupPierre();              //slot pour le coup Pierre
-    void coupCiseau();              //slot pour le coup ciseau
-    void coupPapier();              //slot pour le coup papier
-    void coupMachine(int);          //slot pour le coup effectuer par la machine
+///* ---- PARTIE MODèLE ---------------------------
 
+///* Une définition de type énuméré
+public:
+    enum UnCoup {pierre, papier, ciseau, rien};
+///* Méthodes du Modèle
+public:
+    // Getters
+    UnCoup getCoupJoueur();
+    /* retourne le dernier coup joué par le joueur */
+    UnCoup getCoupMachine();
+    /* retourne le dernier coup joué par le joueur */
+    unsigned int getScoreJoueur();
+    /* retourne le score du joueur */
+    unsigned int getScoreMachine();
+    /* retourne le score de la machine */
+    char determinerGagnant();
+    /* détermine le gagnant 'J' pour joueur, 'M' pour machine, 'N' pour match nul
+                       en fonction du dernier coup joué par chacun d'eux */
+///* Méthodes utilitaires du Modèle
+private :
+    UnCoup genererUnCoup();
+    /* retourne une valeur aléatoire = pierre, papier ou ciseau. Utilisée pour faire jouer la machine */
+// Setters
+
+public:
+    void setCoupJoueur(int);
+    /* initialise l'attribut coupJoueur avec la valeur du paramètre p_coup */
+    void setCoupMachine();
+    /* initialise l'attribut coupmachine avec la valeur du paramètre p_coup */
+    void setScoreJoueur(unsigned int p_score);
+    /* initialise l'attribut scoreJoueur avec la valeur du paramètre p_score */
+    void setScoreMachine(unsigned int p_score);
+    /* initialise l'attribut coupMachine avec la valeur du paramètre p_score */
+
+// Autres modificateurs
+    void majScores(char p_gagnant);
+    /* Mise à jour des scores en fonction des règles de gestion actuelles :
+               - 1 point pour le gagnant lorsqu'il y a un gagnant
+               - 0 point en cas de match nul
+        */
+    void initScores();
+    /* initialise à 0 les attributs scoreJoueur et scoreMachine NON indispensable */
+    void initCoups();
+    /* initialise à rien les attributs coupJoueur et coupMachine NON indispensable */
+    void deroulementPartie();
+
+private slots:
+    void demarerPartie();
+    void coupPierre();
+    void coupCiseau();
+    void coupPapier();
+
+///* Attributs du Modèle
 private:
     Ui::chifoumiVue *ui;
-    int randMinMax(int, int);       //fonction qui génère un nombre aléatoire
-    char determineGagnat(int,int);  //Fonction qui determine le gagnant entre le joueur et la machine
+    QString valMachineScore;        //Decalarartion d'un variable QString pour contenir la valeur du score de la machine
+    QString valJoueurScore;
+    unsigned int scoreJoueur=0;     // score actuel du joueur
+    unsigned int scoreMachine=0;    // score actuel de la Machine
+    UnCoup coupJoueur;              // dernier coup joué par le joueur
+    UnCoup coupMachine;             // dernier coup joué par la machine
+
+
 };
 #endif // CHIFOUMIVUE_H
